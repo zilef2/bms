d3d6e1416c40ff8ad512d9fc9168e5c37ec84e44
Tony Lea <tony@devdojo.com>
2025-02-22 14:38:11 -0500

    updating the logo

--- resources/js/components/AppLogo.vue
+++ resources/js/components/AppLogo.vue
@@ -10,7 +10,7 @@ defineProps<Props>();
 
 <template>
     <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-md">
-        <AppLogoIcon className="size-[1.2rem] fill-current text-white dark:text-black" />
+        <AppLogoIcon className="size-5 fill-current text-white dark:text-black" />
     </div>
     <div className="ml-1 grid flex-1 text-left text-sm">
         <span className="mb-0.5 truncate leading-none font-semibold">Laravel Starter Kit</span>
