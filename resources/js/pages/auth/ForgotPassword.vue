c035ae3e217b42e7f555ba263dd378bbdeb47c20
Tony Lea <tony@devdojo.com>
2025-02-22 16:34:58 -0500

    Adding placeholder to forgot password view

--- resources/js/pages/auth/ForgotPassword.vue
+++ resources/js/pages/auth/ForgotPassword.vue
@@ -33,7 +33,7 @@ const submit = () => {
             <form @submit.prevent="submit">
                 <div class="grid gap-2">
                     <Label for="email">Email address</Label>
-                    <Input id="email" type="email" name="email" autocomplete="off" v-model="form.email" autofocus />
+                    <Input id="email" type="email" name="email" autocomplete="off" v-model="form.email" autofocus placeholder="email@example.com" />
                     <InputError :message="form.errors.email" />
                 </div>
 
