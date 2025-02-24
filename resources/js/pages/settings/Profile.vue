be15c9bf293d33493a37dc7c60d7d72bbf1bde47
Taylor Otwell <taylor@laravel.com>
2025-02-24 14:30:52 -0600

    Revert "fix: update autocomplete attribute in Profile.vue (#29)" (#30)
    This reverts commit a67bfbbee649ba96c6e5ab5fda6b800adaba884f.

--- resources/js/pages/settings/Profile.vue
+++ resources/js/pages/settings/Profile.vue
@@ -65,7 +65,7 @@ const submit = () => {
                             class="mt-1 block w-full"
                             v-model="form.email"
                             required
-                            autocomplete="email"
+                            autocomplete="username"
                             placeholder="Email address"
                         />
                         <InputError class="mt-2" :message="form.errors.email" />
