9174c4404e8454fc45bf1988fac4e3ce886e931a
Taylor Otwell <taylor@laravel.com>
2025-02-23 19:15:50 -0600

    formatting

--- resources/js/pages/auth/VerifyEmail.vue
+++ resources/js/pages/auth/VerifyEmail.vue
@@ -20,12 +20,12 @@ const submit = () => {
     <AuthLayout title="Verify email" description="Please verify your email address by clicking on the link we just emailed to you.">
         <Head title="Email verification" />
 
-        <div class="mb-4 text-sm text-gray-600">
+        <div class="mb-4 text-center text-sm text-gray-600">
             Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you
             didn't receive the email, we will gladly send you another
         </div>
 
-        <div v-if="status === 'verification-link-sent'" class="mb-4 text-sm font-medium text-green-600">
+        <div v-if="status === 'verification-link-sent'" class="mb-4 text-center text-sm font-medium text-green-600">
             A new verification link has been sent to the email address you provided during registration
         </div>
 
