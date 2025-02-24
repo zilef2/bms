3a6dbc4bcd018d3be84a6903830e03053d764f9c
Tony Lea <tony@devdojo.com>
2025-02-23 19:07:19 -0500

    updating className to class

--- resources/js/components/AppLogo.vue
+++ resources/js/components/AppLogo.vue
@@ -12,7 +12,7 @@ defineProps<Props>();
     <div class="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-md">
         <AppLogoIcon class="size-5 fill-current text-white dark:text-black" />
     </div>
-    <div className="ml-1 grid flex-1 text-left text-sm">
+    <div class="ml-1 grid flex-1 text-left text-sm">
         <span class="mb-0.5 truncate leading-none font-semibold">Laravel Starter Kit</span>
     </div>
 </template>
