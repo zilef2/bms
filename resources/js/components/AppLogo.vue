72f41ae9e86f444518e34012666b699c2c36a953
Tony Lea <tony@devdojo.com>
2025-02-23 19:04:20 -0500

    Adding correct class names

--- resources/js/components/AppLogo.vue
+++ resources/js/components/AppLogo.vue
@@ -9,10 +9,10 @@ defineProps<Props>();
 </script>
 
 <template>
-    <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-md">
-        <AppLogoIcon className="size-5 fill-current text-white dark:text-black" />
+    <div class="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-md">
+        <AppLogoIcon class="size-5 fill-current text-white dark:text-black" />
     </div>
     <div className="ml-1 grid flex-1 text-left text-sm">
-        <span className="mb-0.5 truncate leading-none font-semibold">Laravel Starter Kit</span>
+        <span class="mb-0.5 truncate leading-none font-semibold">Laravel Starter Kit</span>
     </div>
 </template>
