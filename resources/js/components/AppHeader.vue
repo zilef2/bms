c22e63ed6384af858712016dd5359f975a4294b5
Faisal Ahmed <fftfaisal@gmail.com>
2025-02-25 20:17:24 +0600

    Fix: Vue type checking for AvatarImage component (#33)

--- resources/js/components/AppHeader.vue
+++ resources/js/components/AppHeader.vue
@@ -166,7 +166,7 @@ const rightNavItems: NavItem[] = [
                                 class="relative size-10 w-auto rounded-full p-1 focus-within:ring-2 focus-within:ring-primary"
                             >
                                 <Avatar class="size-8 overflow-hidden rounded-full">
-                                    <AvatarImage :src="auth.user.avatar" :alt="auth.user.name" />
+                                    <AvatarImage v-if="auth.user.avatar" :src="auth.user.avatar" :alt="auth.user.name" />
                                     <AvatarFallback class="rounded-lg bg-neutral-200 font-semibold text-black dark:bg-neutral-700 dark:text-white">
                                         {{ getInitials(auth.user?.name) }}
                                     </AvatarFallback>
